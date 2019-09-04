341012bce8b4eb5dc737deef19cc00d2b361bb87
TaishiM <liverpool1407@g.ecc.u-tokyo.ac.jp>
2019-09-04 18:32:20 +0900

    enable to get slack user name

--- go.mod
+++ go.mod
@@ -1,3 +1,6 @@
 module github.com/bokusunny/bokuweather
 
-require github.com/aws/aws-lambda-go v1.11.1
+require (
+	github.com/aws/aws-lambda-go v1.11.1
+	github.com/aws/aws-sdk-go v1.21.1 // indirect
+)
--- main.go
+++ main.go
@@ -1,11 +1,46 @@
 package main
 
 import (
+	"encoding/json"
+	"io/ioutil"
+	"net/http"
+	"os"
+
 	"github.com/aws/aws-lambda-go/lambda"
 )
 
+type response struct {
+	Ok      bool `json:"ok"`
+	Profile struct {
+		DisplayName string `json:"display_name"`
+	} `json:"profile"`
+}
+
 func hello() (string, error) {
-	return "Hello, world!", nil
+	rootURL := "https://slack.com/api/"
+	apiMethod := "users.profile.get"
+	token := os.Getenv("SLACK_TOKEN")
+
+	resp, err := http.Get(rootURL + apiMethod + "?token=" + token)
+
+	if err != nil {
+		return err.Error(), nil
+	}
+
+	defer resp.Body.Close()
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return err.Error(), nil
+	}
+
+	var respJSON response
+	if err = json.Unmarshal(body, &respJSON); err != nil {
+		return err.Error(), nil
+	}
+
+	userName := respJSON.Profile.DisplayName
+
+	return userName, nil
 }
 
 func main() {
